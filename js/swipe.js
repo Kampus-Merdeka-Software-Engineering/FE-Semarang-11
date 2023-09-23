d58e5a2a42b865543002424e8ae7184207bd1c75
SYALVA SYADILA <126313282+syalvash@users.noreply.github.com>
2023-09-23 22:45:49 +0700

    swipe.js

--- js/swipe.js
+++ js/swipe.js
@@ -1,28 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const cardContainer = document.querySelector('.card-container');
-    let isDragging = false;
-    let startX, scrollLeft;
-  
-    cardContainer.addEventListener('mousedown', (e) => {
-      isDragging = true;
-      startX = e.pageX - cardContainer.offsetLeft;
-      scrollLeft = cardContainer.scrollLeft;
-    });
-  
-    cardContainer.addEventListener('mouseleave', () => {
-      isDragging = false;
-    });
-  
-    cardContainer.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
-  
-    cardContainer.addEventListener('mousemove', (e) => {
-      if (!isDragging) return;
-      e.preventDefault();
-      const x = e.pageX - cardContainer.offsetLeft;
-      const walk = (x - startX) * 2; // Adjust the sensitivity as needed
-      cardContainer.scrollLeft = scrollLeft - walk;
-    });
+  const cardWrapper = document.querySelector('.card-wrapper');
+  const prevButton = document.querySelector('.carousel-button.prev');
+  const nextButton = document.querySelector('.carousel-button.next');
+
+  const cardWidth = document.querySelector('.card').offsetWidth;
+  let currentIndex = 0;
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  function goToCard(index) {
+      cardWrapper.style.transform = `translateX(-${index * cardWidth}px)`;
+      currentIndex = index;
+
+      // Mengecek apakah kita berada di kartu pertama atau terakhir
+      // dan menonaktifkan atau mengaktifkan tombol-tombol sesuai kebutuhan
+      prevButton.disabled = currentIndex === 0;
+      nextButton.disabled = currentIndex === cardWrapper.children.length - 1;
+  }
+
+  // Fungsi untuk menghandle swipe pada perangkat sentuh
+  cardWrapper.addEventListener('touchstart', function (event) {
+      touchStartX = event.touches[0].clientX;
   });
-  
+
+  cardWrapper.addEventListener('touchmove', function (event) {
+      touchEndX = event.touches[0].clientX;
+  });
+
+  cardWrapper.addEventListener('touchend', function () {
+      const swipeDistance = touchStartX - touchEndX;
+
+      if (swipeDistance > 50 && currentIndex < cardWrapper.children.length - 1) {
+          goToCard(currentIndex + 1);
+      } else if (swipeDistance < -50 && currentIndex > 0) {
+          goToCard(currentIndex - 1);
+      }
+  });
+
+  nextButton.addEventListener('click', function () {
+      if (currentIndex < cardWrapper.children.length - 1) {
+          goToCard(currentIndex + 1);
+      }
+  });
+
+  prevButton.addEventListener('click', function () {
+      if (currentIndex > 0) {
+          goToCard(currentIndex - 1);
+      }
+  });
+});
